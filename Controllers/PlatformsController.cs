8e90ac17b502ebd52a07252cbfcda313ce7c249e
Sidhant Sethi <sidhantsethi738@gmail.com>
2021-10-07 22:36:23 +0530

    7oct

---
+++ Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            System.Console.WriteLine("Inbount Test ==>");
+            return Ok("test Inbound Connection");
+        }
+    }
+}
